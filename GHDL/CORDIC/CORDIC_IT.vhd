e2d1af2353af110a9eda6b80290b2affff2250c3
Cecilia <cecilia.carbonaro@gmail.com>
2019-10-01 16:28:38 -0300

    Bloque CORDIC: Con entidades separadas para iteraciones

--- GHDL/CORDIC/CORDIC_IT.vhd
+++ GHDL/CORDIC/CORDIC_IT.vhd
@@ -2,42 +2,48 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity CORDIC_IT is
     Generic (   N_bits : natural := 8;
-		IT : natural := 0;
-		angle_step : signed(31 downto 0) := "00100000000000000000000000000000"; 
+		iteration : natural := 0;
+		angle_step : signed(31 downto 0) := "00100000000000000000000000000000" 
                 );
     Port ( clk : in STD_LOGIC;
-	   --grados : in STD_LOGIC_VECTOR (N_bits-1 downto 0);
-           --sentido : in STD_LOGIC;
-	   ai : in STD_LOGIC_VECTOR (N_bits-1 downto 0);
-	   xi : in STD_LOGIC_VECTOR (N_bits-1 downto 0);
-	   yi : in STD_LOGIC_VECTOR (N_bits-1 downto 0);
-	   ao : out STD_LOGIC_VECTOR (N_bits-1 downto 0);
-           xo : out STD_LOGIC_VECTOR (N_bits-1 downto 0);
-           yo : out STD_LOGIC_VECTOR (N_bits-1 downto 0);
+	   ai : in STD_LOGIC_VECTOR (N_bits downto 0);
+	   xi : in STD_LOGIC_VECTOR (N_bits downto 0);
+	   yi : in STD_LOGIC_VECTOR (N_bits downto 0);
+	   ao : out STD_LOGIC_VECTOR (N_bits downto 0);
+           xo : out STD_LOGIC_VECTOR (N_bits downto 0);
+           yo : out STD_LOGIC_VECTOR (N_bits downto 0)
            );
 end CORDIC_IT;
-architecture Behavioral of CORDIC is
-   begin
-    process(clk)
+architecture Behavioral of CORDIC_IT is
     begin
-	if rising_edge(clk) then
-		if ai > 0 then
-                    ao <= ai - angle_step(31 downto 32-N_bits-1);
-                    xo <= xi - shift_right(yi,IT);
-                    yo <= yi + shift_right(xi,IT);
-                else
-		    if ai <= 0 then
-	                ao <= ai + angle_step(31 downto 32-N_bits-1);
-                	xo <= xi + shift_right(yi,IT);
-                        yo <= yi - shift_right(xi,IT);
-                end if;
-	    end if;
+    process(clk)
+	begin
+  	if iteration=0 then
+		if rising_edge(clk) then
+			if signed(ai)>0 then
+				ao <= STD_LOGIC_VECTOR(signed(ai) - resize(angle_step(31 downto 32-N_bits),N_bits+1));
+     				xo <= STD_LOGIC_VECTOR(signed(xi) - signed(yi));
+    				yo <= STD_LOGIC_VECTOR(signed(yi) + signed(xi));
+  			else   
+				ao <= STD_LOGIC_VECTOR(signed(ai) + resize(angle_step(31 downto 32-N_bits),N_bits+1));
+     				xo <= STD_LOGIC_VECTOR(signed(xi) + signed(yi));
+      				yo <= STD_LOGIC_VECTOR(signed(yi) - signed(xi));
+   			end if;
+		end if;
+ 	else
+		if rising_edge(clk) then
+			if signed(ai) > 0 then
+                    	ao <= STD_LOGIC_VECTOR(signed(ai) - resize(angle_step(31 downto 32-N_bits-1),N_bits+1));
+                    	xo <= STD_LOGIC_VECTOR(signed(xi) - shift_right(signed(yi),iteration));
+                    	yo <= STD_LOGIC_VECTOR(signed(yi) + shift_right(signed(xi),iteration));
+                	else
+	            	ao <= STD_LOGIC_VECTOR(signed(ai) + resize(angle_step(31 downto 32-N_bits-1),N_bits+1));
+               	    	xo <= STD_LOGIC_VECTOR(signed(xi) + shift_right(signed(yi),iteration));
+                    	yo <= STD_LOGIC_VECTOR(signed(yi) - shift_right(signed(xi),iteration));
+                	end if;
+		end if;
+	end if;
     end process;
 end Behavioral;
